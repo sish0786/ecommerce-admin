d4c6d2d6cc0217122b58ad9f6b40923e3aad2286
Sheharoze <sheharozuddin@gmail.com>
2024-03-08 04:02:38 -0500

    Initialized the project using next.js Setup google (google console) authentication using next auth Setup mongodb

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+
+export default function Home() {
+  const { data: session } = useSession();
+  if(!session){
+    return (<div className="bg-blue-900 w-screen h-screen flex items-center">
+    <div className="w-full text-center">
+      <button onClick={()=>{signIn('google')}} className="bg-white p-2 rounded-lg px-4">Login with Google</button>
+    </div>
+  </div>);
+  }
+  return (<div>Logged In {session.user.email}</div>)
+}
