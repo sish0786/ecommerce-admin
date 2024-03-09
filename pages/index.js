b4f8b4314bf8613e1b3821a2da2234051f9fa49f
Sheharoze <sheharozuddin@gmail.com>
2024-03-08 19:22:15 -0500

    productManagement; Added Product View Product Edit Created APi's Added mongoose

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,18 @@
+import Layout from "@/components/Layout";
+import Nav from "@/components/Nav";
 import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
-  const { data: session } = useSession();
-  if(!session){
-    return (<div className="bg-blue-900 w-screen h-screen flex items-center">
-    <div className="w-full text-center">
-      <button onClick={()=>{signIn('google')}} className="bg-white p-2 rounded-lg px-4">Login with Google</button>
+  const {data: session} = useSession();
+  return (<Layout>
+    <div className="text-blue-900 flex justify-between">
+        <h2>Hello, <b>{session?.user?.name}</b></h2>
+        <div className="flex bg-gray-300 text-black rounded-lg overflow-hidden">
+          <img src={session?.user?.image} alt="" className="w-6 h-6"></img>
+          <span className=" px-2">
+          {session?.user?.name}
+          </span>
+        </div>
     </div>
-  </div>);
-  }
-  return (<div>Logged In {session.user.email}</div>)
+  </Layout>)
 }
